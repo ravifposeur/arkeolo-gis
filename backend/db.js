4f7487f24cd68ab2bb9003bee04ea5c35a2f0f99
ravifposeur <ravifgayuhwicaksono@gmail.com>
2025-12-05 22:49:13 +0700

    fix: fixing env variable for db

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,22 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    // Gunakan variabel DATABASE_URL yang nanti kita set di Vercel
-    connectionString: process.env.DATABASE_URL, 
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: false // Wajib untuk Neon
+        rejectUnauthorized: false // Wajib true/objek untuk database cloud (Neon)
+    }
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('❌ Error koneksi Database:', err.message);
+    } else {
+        console.log('✅ Berhasil terhubung ke Database!');
+        release();
     }
 });
 
