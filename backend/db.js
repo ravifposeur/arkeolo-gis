8ccaa7c106ba238e60a57282bdbb35ecaa11387d
ravifposeur <ravifgayuhwicaksono@gmail.com>
2025-12-05 22:52:06 +0700

    fix: fixing env variable for db

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,35 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false // Wajib true/objek untuk database cloud (Neon)
-    }
-});
+let config;
+
+if (process.env.DATABASE_URL) {
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false 
+        }
+    };
+} else {
+    config = {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        ssl: {
+            rejectUnauthorized: false 
+        }
+    };
+}
+
+const pool = new Pool(config);
 
 pool.connect((err, client, release) => {
     if (err) {
-        console.error('❌ Error koneksi Database:', err.message);
+        console.error('❌ Gagal konek DB:', err.message);
     } else {
-        console.log('✅ Berhasil terhubung ke Database!');
+        console.log('✅ Berhasil konek ke Database Neon!');
         release();
     }
 });
